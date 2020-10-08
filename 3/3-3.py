d844f83041a86aa31a04130bab8029370e833a03
taeyoung831@gist.ac.kr <taeyoung831@gist.ac.kr>
2020-10-08 14:37:28 +0900

    first commit

---
+++ 3/3-3.py
@@ -0,0 +1,9 @@
+# return : 반환의 의미, 반환 후의 함수를 종료하는 기능도 있다!!
+def recursive_function(i):
+    if i==100:
+        return
+    print(i,'번째 재귀 함수에서',i+1,'번째 재귀 함수를 호출합니다.')
+    recursive_function(i+1)
+    print(i,'번째 재귀 함수를 종료합니다.')
+
+recursive_function(1)
